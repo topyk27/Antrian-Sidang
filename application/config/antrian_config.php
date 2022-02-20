b23a36450920d5bc865357d2ebffff8f512f26e4
topyk27 <taufikdwiwahyuputra@gmail.com>
2022-02-20 15:59:03 +0800

    Update antrian_config.php

--- application/config/antrian_config.php
+++ application/config/antrian_config.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['version']='1.0.0';
 // $config['database_sipp']='sipp';
 $config['database_sipp']='rkYJiHGEL7'; //testing
-$config['panggil']= 'pc'; // luar | pc
+$config['panggil']= 'luar'; // luar | pc
 //luar = pc monitor yang berbunyi
 //pc = pc ruang sidang yang berbunyi
 
